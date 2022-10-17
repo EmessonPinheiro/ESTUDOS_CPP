e4f9c97bf1195184a7071d67406714b0be76897c
Emesson Sousa Pinheiro <elbread2.0@gmail.com>
2022-10-17 10:32:59 -0300

    testando
    tentando rodar o c++

--- project/variaveis.cpp
+++ project/variaveis.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int main(){
+int main()
+{
 
-    //TIPO NOME;
-    //TIPO NOME = VALOR;
+cout <<"hello world" <<endl;
 
-    int vidas; //87
-    char letra; //'h'
-    char letras[]; //algum texto e cada letra em uma posição do vetor
-    double decimal; //2.599999
-    float decimal2; //2.6
-    bool vivo; //true or false
-    string palavra; // "caguei"
-
+return 0;
 }
