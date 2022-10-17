58e27bb16bb02adfe7f0c8df3e99200841a57abf
Emesson Sousa Pinheiro <elbread2.0@gmail.com>
2022-10-17 10:26:09 -0300

    aula sobre variaveis
    coisas da aula4

---
+++ project/variaveis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //TIPO NOME;
+    //TIPO NOME = VALOR;
+
+    int vidas; //87
+    char letra; //'h'
+    char letras[]; //algum texto e cada letra em uma posição do vetor
+    double decimal; //2.599999
+    float decimal2; //2.6
+    bool vivo; //true or false
+    string palavra; // "caguei"
+
+}
